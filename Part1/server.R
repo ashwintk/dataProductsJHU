d62a9f1a77593b490dd876084a8e8dbb76d3c18a
Ashwin Kumar <tashwin@okstate.edu>
2014-11-25 01:38:18 -0600

    Finished Part1

--- Part1/server.R
+++ Part1/server.R
@@ -83,39 +83,83 @@ generateBMIPlotData <- function(type,bmi){
       text(4,120,"Underweight",cex=2,srt=18)
     }
   }
-  
 }
+#This method computes base metabolic rate based on Mifflin - St Jeor equation
+computeBMR <- function(heightInCentiMeter,weightInKilograms,sex,age){
+  if(sex=="Male"){
+    10*weightInKilograms + 6.25*heightInCentiMeter - 5*age + 5
+  }else{
+    10*weightInKilograms + 6.25*heightInCentiMeter - 5*age -161
+  }
+}
+#This method returns float values corresponding to the activity indicators
+getActivityFactor <- function(act_ind){
+  if(act_ind=="1")#Basal Metabolic Rate (BMR)
+    1
+  else if(act_ind=="2")#Sedentary - Little or no exercise and no desk job
+    1.2
+  else if(act_ind=="3")#Lightly Active - Light exercise or sports 1-3 days a week
+    1.375
+  else if(act_ind=="4")#Moderately Active - Moderate exercise or sports 3-5 days a week
+    1.55
+  else if(act_ind=="5")#Very Active - Hard exercise or sports 6-7 days a week
+    1.725
+  else if(act_ind=="6")#Extremely Active - Hard daily exercise or sports and physical job
+    1.9
+}
+
 library(shiny)
+library(rCharts)
+
 shinyServer(
   function(input,output){
     output$bmiTbl <- renderTable({generateBMITable()}) #Display BMI Category table
     observe({
-     if(input$tab_panel=="Standard"){#if unit of measurement is standard
-       hieghtInInches <- (as.numeric(input$feet)*12)+as.numeric(input$inches) #convert height to inches
-       bmi <- computeBMI(hieghtInInches,as.numeric(input$pounds),1) #compute BMI
-       if(is.nan(bmi)){ #if BMI is NaN
-         output$outputBMI <- renderPrint({"Check the values you have entered and try again"})
-       }else{ #Display User entered values, BMI, their BMI category and plot      
-         output$inputHeight <- renderPrint({paste("Height: ",input$feet,"feet,",input$inches,"inches",sep=" ")})
-         output$inputWeight <- renderPrint({paste("Weight: ",input$pounds,"pounds",sep=" ")})
-         output$outputBMI <- renderPrint({round(bmi,1)})
-         output$bmiCategory <- renderPrint({assesBMICategory(bmi)})
-         output$bmiPlot <- renderPlot({generateBMIPlotData(1,bmi)})
-       }
-     }else{#if unit of measurement is metric
-       hieghtInMeters <- as.numeric(input$height)/100 #convert hieght to meters
-       bmi <- computeBMI(hieghtInMeters,as.numeric(input$weight),2)#compute BMI
-       if(is.nan(bmi)){#if BMI is NaN
-         output$outputBMI <- renderPrint({"Check the values you have entered and try again"})
-       }else{#Display User entered values, BMI, their BMI category and plot
-         output$inputHeight <- renderPrint({paste("Height: ",input$height,"centimeters",sep=" ")})
-         output$inputWeight <- renderPrint({paste("Weight: ",input$weight,"kilograms",sep=" ")})
-         output$outputBMI <- renderPrint({round(bmi,1)})
-         output$bmiCategory <- renderPrint({assesBMICategory(bmi)})
-         output$bmiPlot <- renderPlot({generateBMIPlotData(2,bmi)})
-       }
-     }
-    })
-    
+      if(input$typeOfOutput=="BMI Calculations"){
+        if(input$tab_panel=="Standard"){#if unit of measurement is standard
+          hieghtInInches <- (as.numeric(input$feet)*12)+as.numeric(input$inches) #convert height to inches
+          bmi <- computeBMI(hieghtInInches,as.numeric(input$pounds),1) #compute BMI
+          if(is.nan(bmi)){ #if BMI is NaN
+            output$outputBMI <- renderPrint({"Check the values you have entered and try again"})
+          }else{ #Display User entered values, BMI, their BMI category and plot      
+            output$inputHeight <- renderPrint({paste("Height: ",input$feet,"feet,",input$inches,"inches",sep=" ")})
+            output$inputWeight <- renderPrint({paste("Weight: ",input$pounds,"pounds",sep=" ")})
+            output$outputBMI <- renderPrint({round(bmi,1)})
+            output$bmiCategory <- renderPrint({assesBMICategory(bmi)})
+            output$bmiPlot <- renderPlot({generateBMIPlotData(1,bmi)})
+          }
+        }else{#if unit of measurement is metric
+          hieghtInMeters <- as.numeric(input$height)/100 #convert hieght to meters
+          bmi <- computeBMI(hieghtInMeters,as.numeric(input$weight),2)#compute BMI
+          if(is.nan(bmi)){#if BMI is NaN
+            output$outputBMI <- renderPrint({"Check the values you have entered and try again"})
+          }else{#Display User entered values, BMI, their BMI category and plot
+            output$inputHeight <- renderPrint({paste("Height: ",input$height,"centimeters",sep=" ")})
+            output$inputWeight <- renderPrint({paste("Weight: ",input$weight,"kilograms",sep=" ")})
+            output$outputBMI <- renderPrint({round(bmi,1)})
+            output$bmiCategory <- renderPrint({assesBMICategory(bmi)})
+            output$bmiPlot <- renderPlot({generateBMIPlotData(2,bmi)})
+          }
+        }
+      }else if(input$typeOfOutput=="Daily Calorie Intake Estimator"){
+        heightInCentiMeters <- 0
+        weightInKgs <- 0
+        if(input$tab_panel=="Standard"){
+          heightInCentiMeters <- ((as.numeric(input$feet)*12)+as.numeric(input$inches)) *2.54
+          weightInKgs <- as.numeric(input$pounds)*0.453592
+        }else{
+          heightInCentiMeters <- as.numeric(input$height)
+          weightInKgs <- as.numeric(input$weight)
+        }
+        BMR <- computeBMR(heightInCentiMeters,weightInKgs,input$sex,input$age)
+        activityFactor <- getActivityFactor(input$activity_indicator)
+        output$maintainWeight <- renderPrint({paste("You need ",round(BMR*activityFactor,0)," calories/day to maintain your weight",sep="")})
+        output$looseOneLBPerWeek <- renderPrint({paste("You need ",round(BMR*activityFactor,0) - 500," calories/day to loose 1 lb/week",sep="")})
+        output$looseTwoLBPerWeek <- renderPrint({paste("You need ",round(BMR*activityFactor,0) - 1000," calories/day to loose 2 lb/week",sep="")})
+        output$gainOneLBPerWeek <- renderPrint({paste("You need ",round(BMR*activityFactor,0) + 500," calories/day to gain 1 lb/week",sep="")})
+        output$gainTwoLBPerWeek <- renderPrint({paste("You need ",round(BMR*activityFactor,0) + 1000," calories/day to gain 2 lb/week",sep="")})
+      }   
+    }
+    )
   }
-  )
+)
--- Part1/ui.R
+++ Part1/ui.R
@@ -2,27 +2,27 @@ library(shiny)
 
 shinyUI(
   pageWithSidebar(#Page with sidebar
-    headerPanel("Body Mass Index Calculator"), #Heading
+    headerPanel("Body Mass Index and daily calorie intake calculator"), #Heading
     sidebarPanel(
       tabsetPanel(id = "tab_panel", #Two tabs one each for units of measurement
-        tabPanel(#Standard
-          title = "Standard",
-          h4("Enter your height in feet & inches"),
-          numericInput('feet','feet:',0,min = 1,max=20,step=1),
-          numericInput('inches','inches',0,min = 1,max=11,step=1),
-          h4("Enter your weight in pounds"),
-          numericInput(inputId = 'pounds',label = "", value = 0),
-          submitButton('Compute your BMI')
-          ),
-        tabPanel(#Metric
-          title = "Metric",
-          h4("Enter your height centimeters"),
-          numericInput('height',label = "",value = 0),
-          h4("Enter your weight in kilograms"),
-          numericInput('weight',label = "",value = 0),
-          submitButton('Compute your BMI')
-          )
-        ),
+                  tabPanel(#Standard
+                    title = "Standard",
+                    h4("Enter your height in feet & inches"),
+                    numericInput('feet','feet:',0,min = 1,max=20,step=1),
+                    numericInput('inches','inches',0,min = 1,max=11,step=1),
+                    h4("Enter your weight in pounds"),
+                    numericInput(inputId = 'pounds',label = "", value = 0),
+                    submitButton('Compute your BMI')
+                  ),
+                  tabPanel(#Metric
+                    title = "Metric",
+                    h4("Enter your height centimeters"),
+                    numericInput('height',label = "",value = 0),
+                    h4("Enter your weight in kilograms"),
+                    numericInput('weight',label = "",value = 0),
+                    submitButton('Compute your BMI')
+                  )
+      ),
       br(),
       h4("BMI Table"),
       tableOutput("bmiTbl"), #Display BMI Range as table
@@ -38,19 +38,62 @@ shinyUI(
         tags$li("History of heart diseases"),
         tags$li("Physical inactivity"),
         tags$li("Cigarette smoking")
-        )
-      ), mainPanel(#Main Panel, to display results and graph
-        h3("Results of your BMI computation are as follows:"),
-        br(),
-        h4("You have entered the following values"),
-        verbatimTextOutput("inputHeight"),
-        verbatimTextOutput("inputWeight"),
-        h4("Your BMI is"),
-        verbatimTextOutput("outputBMI"),
-        h4("Your BMI Category is"),
-        uiOutput("bmiCategory"),
-        br(),
-        plotOutput("bmiPlot",height = 500,width = 500)
-        )
+      ),br(),br(),
+      tags$a(href="index.html","User guide can be found here")
+    ), mainPanel(#Main Panel, to display results and graph
+      tabsetPanel(id = "typeOfOutput", #Two tabs one each for units of measurement
+                  tabPanel(#BMI Calculations
+                    title = "BMI Calculations",
+                    h3("Results of your BMI computation are as follows:"),
+                    br(),
+                    h4("You have entered the following values"),
+                    verbatimTextOutput("inputHeight"),
+                    verbatimTextOutput("inputWeight"),
+                    h4("Your BMI is"),
+                    verbatimTextOutput("outputBMI"),
+                    h4("Your BMI Category is"),
+                    uiOutput("bmiCategory"),
+                    br(),
+                    plotOutput("bmiPlot",height = 500,width = 500),
+                    br(),
+                    br(),
+                    tags$a(href="http://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmicalc.htm","Formula for BMI calculations and BMI categories have been gathered from National Health Institute")
+                  ),tabPanel(#Calorie Calculations
+                    title="Daily Calorie Intake Estimator",
+                    h3("Enter the following Information to calculate your daily calorie intake"),
+                    br(),
+                    radioButtons(inputId = "sex",label = "Sex: ",c("Male" = "Male","Female" = "Female")), br(),
+                    numericInput(inputId = "age",label = "Age",1,min = 1,max = 100,step = 1),br(),
+                    radioButtons(inputId = "activity_indicator",label = "Choose relevant activity",c(
+                      "Basal Metabolic Rate (BMR)"="1",
+                      "Sedentary - Little or no exercise and no desk job"="2",
+                      "Lightly Active - Light exercise or sports 1-3 days a week"="3",
+                      "Moderately Active - Moderate exercise or sports 3-5 days a week"="4",
+                      "Very Active - Hard exercise or sports 6-7 days a week" = "5",
+                      "Extremely Active - Hard daily exercise or sports and physical job"="6"
+                    )), br(),
+                    submitButton("Compute your daily required calorie intakes"),
+                    h3("The following are your calorie intake results based on your input"),
+                    verbatimTextOutput("maintainWeight"),
+                    verbatimTextOutput("looseOneLBPerWeek"),
+                    verbatimTextOutput("looseTwoLBPerWeek"),
+                    verbatimTextOutput("gainOneLBPerWeek"),
+                    verbatimTextOutput("gainTwoLBPerWeek"),br(),
+                    tags$p("The following are sources which were referred in computation of BMR and Daily calorie Requirements"),
+                    tags$ul(
+                      tags$ol(
+                        tags$a(href="http://en.wikipedia.org/wiki/Basal_metabolic_rate#BMR_estimation_formulas","1. Basal Metabolic Rate - Mifflin - St Jeor equation")
+                      ),
+                      tags$ol(
+                        tags$a(href="http://www.calculator.net/calorie-calculator.html","2. Calorie Intake Calculator")
+                      ),
+                      tags$ol(
+                        tags$a(href="http://www.caloriesperhour.com/tutorial_BMR.php","3. Calculating BMR and RMR")
+                      )
+                    )
+                  )
+      )
+      
     )
   )
+)
